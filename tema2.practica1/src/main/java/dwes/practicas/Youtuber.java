b6cc0f51097bbbf46a0b20d65c3a7de1b80970f5
David Mallol <david@diemtic.es>
2024-09-23 20:16:49 +0200

    Initial commmit

--- tema2.practica1/src/main/java/dwes/practicas/Youtuber.java
+++ tema2.practica1/src/main/java/dwes/practicas/Youtuber.java
@@ -1,4 +1,16 @@
 package dwes.practicas;
 
-public class Youtuber {
+import java.time.LocalDate;
+import java.util.Date;
+
+public record Youtuber(String name, LocalDate startDate, int subscribers, int views) {
+    @Override
+    public String toString() {
+        return "Youtuber{" +
+                "name='" + name + '\'' +
+                ", startDate=" + startDate +
+                ", subscribers=" + subscribers +
+                ", views=" + views +
+                '}';
+    }
 }
--- tema2.practica1/src/main/java/dwes/practicas/YoutuberService.java
+++ tema2.practica1/src/main/java/dwes/practicas/YoutuberService.java
@@ -1,4 +1,34 @@
 package dwes.practicas;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class YoutuberService {
+    private static final String COMMA_DELIMITER = ",";
+    private List<Youtuber> youtubers;
+
+    public YoutuberService() {
+        this(Path.of("src", "main", "resources", "youtubers.csv"));
+    }
+
+    public YoutuberService(Path youtubersFile) {
+        try (Stream<String> fileContent = Files.lines(youtubersFile)) {
+            parseYoutubers(fileContent);
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public void parseYoutubers(Stream<String> fileContent) {
+        youtubers = fileContent.map(l -> Arrays.asList(l.split(COMMA_DELIMITER)))
+                    .map(l -> new Youtuber(l.get(0), LocalDate.parse(l.get(1)), Integer.parseInt(l.get(2)), Integer.parseInt(l.get(3)))).toList();
+    }
+
+    public List<Youtuber> getYoutubers() {
+        return youtubers;
+    }
 }
